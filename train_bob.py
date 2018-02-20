8e41067b27b59737f3fb4f6f4ffc154eed3e249e
DJ Strouse <danieljstrouse@gmail.com>
2018-02-19 22:54:48 -0500

    bob now loads env_config from alice

--- train_bob.py
+++ train_bob.py
@@ -6,6 +6,7 @@ import pickle
 import datetime
 import importlib
 import glob
+import imp
 from collections import namedtuple
 from shutil import copy
 if "../" not in sys.path: sys.path.append("../") 
@@ -15,15 +16,21 @@ from agents.alice import PolicyEstimator
 from training.REINFORCE_bob import reinforce
 from plotting.plot_episode_stats import *
 from plotting.visualize_grid_world import *
-import env_config
 
 Result = namedtuple('Result', ['alice', 'bob'])
 Stats = namedtuple('Stats', ['episode_lengths', 'episode_rewards', 'episode_kls'])
 
-def train_bob(config_extension = ''):
+def train_bob(bob_config_ext = '', exp_name_ext = ''):
+  
+  # import alice
+  results_directory = os.getcwd()+'/results/'
+  alice_directory = results_directory+alice_experiment+'/'
+  alice_config = imp.load_source('alice_config', alice_directory+'alice_config.py')
+  alice_agent_param, alice_training_param, alice_experiment_name = alice_config.get_config()
   
   # import and init env
-  env_param, experiment_name_ext = env_config.get_config()
+  env_config = imp.load_source('env_config', alice_directory+'env_config.py')
+  env_param, env_exp_name_ext = env_config.get_config()
   env = TwoGoalGridWorld(env_param.shape,
                          env_param.r_correct,
                          env_param.r_incorrect,
@@ -32,19 +39,11 @@ def train_bob(config_extension = ''):
                          env_param.goal_dist)
   
   # import bob
-  config = importlib.import_module('bob_config'+config_extension)
+  config = importlib.import_module('bob_config'+bob_config_ext)
   agent_param, training_param, experiment_name, alice_experiment = config.get_config()
-  experiment_name += experiment_name_ext
+  experiment_name += env_exp_name_ext
+  experiment_name += exp_name_ext
   
-  # import alice
-  results_directory = os.getcwd()+'/results/'
-  alice_directory = results_directory+alice_experiment+'/'
-  cwd = os.getcwd() # save cwd
-  sys.path.append('/results/'+alice_experiment) # move to alice_config directory
-  import alice_config # import alice 
-  os.chdir(cwd) # change back to original wd
-  alice_agent_param, alice_training_param, alice_experiment_name = alice_config.get_config()
-
   # run training, and if nans, creep in, train again until they don't
   success = False
   while not success:
@@ -76,27 +75,28 @@ def train_bob(config_extension = ''):
                                                   bob_goal_access = training_param.bob_goal_access)
       if success:
         # save session
-        experiment_directory = datetime.datetime.now().strftime("%Y_%m_%d_%H%M")+'_'+experiment_name+'/'
+        experiment_directory = datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")+'_'+experiment_name+'/'
         directory = results_directory + experiment_directory
         if not os.path.exists(directory+'bob/'): os.makedirs(directory+'bob/')
         save_path = saver.save(sess, directory+'bob/bob.ckpt')
       else:
         f = open('error.txt','w')
-        d = datetime.datetime.now().strftime("%A, %B %d, %I:%M %p")
+        d = datetime.datetime.now().strftime("%A, %B %d, %I:%M:%S %p")
         f.write("{}: experiment '{}' failed and reran\n".format(d, experiment_name))
         f.close()
   
   # save experiment stats
+  a = Stats(episode_lengths = alice_stats.episode_lengths,
+            episode_rewards = alice_stats.episode_rewards,
+            episode_kls = alice_stats.episode_kls)
+  b = Stats(episode_lengths = bob_stats.episode_lengths,
+            episode_rewards = bob_stats.episode_rewards,
+            episode_kls = bob_stats.episode_kls)
+  result = Result(alice = a, bob = b)
   if not os.path.exists(directory): os.makedirs(directory)
   with open(directory+'results.pkl', 'wb') as output:
     # copy to locally-defined Stats objects to make pickle happy
-    a = Stats(episode_lengths = alice_stats.episode_lengths,
-              episode_rewards = alice_stats.episode_rewards,
-              episode_kls = alice_stats.episode_kls)
-    b = Stats(episode_lengths = bob_stats.episode_lengths,
-              episode_rewards = bob_stats.episode_rewards,
-              episode_kls = bob_stats.episode_kls)
-    pickle.dump(Result(alice = a, bob = b), output, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(result, output, pickle.HIGHEST_PROTOCOL)
   
   # copy config file to results directory to ensure experiment repeatable
   copy(os.getcwd()+'/bob_config'+config_extension+'.py', directory+'bob_config.py')
@@ -115,7 +115,7 @@ def train_bob(config_extension = ''):
                              tick_label = 40,
                              axis_label = 50,
                              title = 60)
-  plot_episode_stats(bob_stats, figure_sizes, smoothing_window = 25, noshow = True, directory = directory)
+  plot_episode_stats(result, figure_sizes, noshow = True, directory = directory)
   print('\n\nResults saved in {}'.format(directory))
   
   return
