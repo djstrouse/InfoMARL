607d80ed8d55e8dbf0f0c7c87182826d43e151b7
DJ Strouse <danieljstrouse@gmail.com>
2018-03-02 19:59:48 -0500

    removed episodes plot, made work with no display, removed eps format, added means to title of steps per reward

--- plotting/plot_episode_stats.py
+++ plotting/plot_episode_stats.py
@@ -1,7 +1,10 @@
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.rcParams['agg.path.chunksize'] = 10000
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from util.stats import rate_last_N, first_time_to
+from util.stats import rate_last_N, mean_last_N, first_time_to
 
 def plot_episode_stats(stats, figure_sizes, noshow = False, directory = None):
   
@@ -27,7 +30,6 @@ def plot_episode_stats(stats, figure_sizes, noshow = False, directory = None):
   plt.title("Episode Length over Time (Smoothed over {} episodes)".format(window), fontsize = figure_sizes.title)
   plt.tick_params(labelsize = figure_sizes.tick_label)
   if directory:
-    plt.savefig(directory+'smoothed_episode_lengths.eps', format='eps')
     plt.savefig(directory+'smoothed_episode_lengths.pdf', format='pdf')
     plt.savefig(directory+'smoothed_episode_lengths.png', format='png')
   if noshow: plt.close(fig0)
@@ -45,7 +47,6 @@ def plot_episode_stats(stats, figure_sizes, noshow = False, directory = None):
   plt.title("Episode Length over Time", fontsize = figure_sizes.title)
   plt.tick_params(labelsize = figure_sizes.tick_label)
   if directory:
-    plt.savefig(directory+'episode_lengths.eps', format='eps')
     plt.savefig(directory+'episode_lengths.pdf', format='pdf')
     plt.savefig(directory+'episode_lengths.png', format='png')
   if noshow: plt.close(fig1)
@@ -65,7 +66,6 @@ def plot_episode_stats(stats, figure_sizes, noshow = False, directory = None):
   plt.title("Episode Reward over Time (Smoothed over window size {})".format(window), fontsize = figure_sizes.title)
   plt.tick_params(labelsize = figure_sizes.tick_label)
   if directory:
-    plt.savefig(directory+'episode_rewards.eps', format='eps')
     plt.savefig(directory+'episode_rewards.pdf', format='pdf')
     plt.savefig(directory+'episode_rewards.png', format='png')
   if noshow: plt.close(fig2)
@@ -92,35 +92,15 @@ def plot_episode_stats(stats, figure_sizes, noshow = False, directory = None):
   plt.title(title, fontsize = figure_sizes.title)
   plt.tick_params(labelsize = figure_sizes.tick_label)
   if directory:
-    plt.savefig(directory+'reward_per_timestep.eps', format='eps')
     plt.savefig(directory+'reward_per_timestep.pdf', format='pdf')
     plt.savefig(directory+'reward_per_timestep.png', format='png')
   if noshow: plt.close(fig3)
   else: plt.show(fig3)
   
-  # Plot episodes per time step
-  fig4 = plt.figure(figsize = figure_sizes.figure)
-  cumulative_steps = np.cumsum(stats.episode_lengths)
-  plt.plot(cumulative_steps, np.arange(len(stats.episode_lengths)), linewidth = 8, label = 'bob')
-  if two_agents:
-    cumulative_steps = np.cumsum(alice.episode_lengths)
-    plt.plot(cumulative_steps, np.arange(len(alice.episode_lengths)), linewidth = 8, label = 'alice')
-    plt.legend(loc = 'upper left', fontsize = figure_sizes.axis_label)
-  plt.xlabel("Time Steps", fontsize = figure_sizes.axis_label)
-  plt.ylabel("Episode", fontsize = figure_sizes.axis_label)
-  plt.title("Episodes per time step", fontsize = figure_sizes.title)
-  plt.tick_params(labelsize = figure_sizes.tick_label)
-  if directory:
-    plt.savefig(directory+'episodes_per_timestep.eps', format='eps')
-    plt.savefig(directory+'episodes_per_timestep.pdf', format='pdf')
-    plt.savefig(directory+'episodes_per_timestep.png', format='png')
-  if noshow: plt.close(fig4)
-  else: plt.show(fig4)
-  
   if stats.episode_kls is not None:
     # Plot a rolling estimate of I(action;goal|state)
     window = 500 # measure in episodes
-    fig5 = plt.figure(figsize = figure_sizes.figure)
+    fig4 = plt.figure(figsize = figure_sizes.figure)
     info_smoothed = pd.Series(np.asarray(stats.episode_kls)/np.asarray(stats.episode_lengths)).rolling(window, min_periods = window).mean()
     plt.plot(cumulative_steps, info_smoothed)
     plt.xlabel("Time Steps", fontsize = figure_sizes.axis_label)
@@ -128,34 +108,42 @@ def plot_episode_stats(stats, figure_sizes, noshow = False, directory = None):
     plt.title("Info estimated over sliding window of {} episodes".format(window), fontsize = figure_sizes.title)
     plt.tick_params(labelsize = figure_sizes.tick_label)
     if directory:
-      plt.savefig(directory+'info.eps', format='eps')
       plt.savefig(directory+'info.pdf', format='pdf')
       plt.savefig(directory+'info.png', format='png')
-    if noshow: plt.close(fig5)
-    else: plt.show(fig5)
-  else: fig5 = None
+    if noshow: plt.close(fig4)
+    else: plt.show(fig4)
+  else: fig4 = None
   
   # Plot time steps per unit reward (smoothed)
   window = 1000
-  fig6 = plt.figure(figsize = figure_sizes.figure)
-  total_steps, steps_per_reward = first_time_to(stats.episode_lengths, stats.episode_rewards)
+  fig5 = plt.figure(figsize = figure_sizes.figure)
+  total_steps, steps_per_reward = first_time_to(stats.episode_lengths, stats.episode_rewards) 
+  N = 10000
+  average_steps_per_reward = mean_last_N(total_steps, steps_per_reward, N = N)
   steps_per_reward_smoothed = pd.Series(steps_per_reward).rolling(window, min_periods = window).mean()
-  plt.plot(total_steps, steps_per_reward_smoothed, color = 'b', label = 'bob', linewidth = 8)
+  if two_agents: lab = 'bob'
+  else: lab = 'alice'
+  plt.plot(total_steps, steps_per_reward_smoothed, color = 'b', label = lab, linewidth = 8)
   if two_agents:
-    average_steps_per_reward = np.sum(alice.episode_lengths)/np.sum(alice.episode_rewards)
-    plt.axhline(y = average_steps_per_reward, color = 'r', label = 'alice', linewidth = 8)
+    average_steps_per_reward_alice = np.sum(alice.episode_lengths)/np.sum(alice.episode_rewards)
+    plt.axhline(y = average_steps_per_reward_alice, color = 'r', label = 'alice', linewidth = 8)
     plt.legend(loc = 'upper right', fontsize = figure_sizes.axis_label)
-  _, ymax = plt.gca().get_ylim()
-  plt.ylim(0, min(6*average_steps_per_reward,ymax))
+    _, ymax = plt.gca().get_ylim()
+    plt.ylim(0, min(6*average_steps_per_reward_alice,ymax))
+    tit = "Smoothed over ~%i episodes, Mean (last %i steps): Bob %.1f, Alice %.1f" % (window, N, average_steps_per_reward, average_steps_per_reward_alice)
+  else:
+    tit = "Smoothed over ~%i episodes, Mean (last %i steps): %.1f" % (window, N, average_steps_per_reward)
+  plt.title(tit, fontsize = figure_sizes.title)
   plt.xlabel("Time Steps", fontsize = figure_sizes.axis_label)
-  plt.ylabel("Time Steps per Reward", fontsize = figure_sizes.axis_label)
-  plt.title("Steps per Reward over Time (Smoothed over approximately {} episodes)".format(window), fontsize = figure_sizes.title) 
+  plt.ylabel("Time Steps per Reward", fontsize = figure_sizes.axis_label)  
   plt.tick_params(labelsize = figure_sizes.tick_label)
   if directory:
-    plt.savefig(directory+'steps_per_reward.eps', format='eps')
     plt.savefig(directory+'steps_per_reward.pdf', format='pdf')
     plt.savefig(directory+'steps_per_reward.png', format='png')
-  if noshow: plt.close(fig6)
-  else: plt.show(fig6)
+  if noshow: plt.close(fig5)
+  else: plt.show(fig5)
 
-  return fig0, fig1, fig2, fig3, fig4, fig5, fig6
+  if two_agents:
+    return average_steps_per_reward, average_steps_per_reward_alice
+  else:
+    return average_steps_per_reward
