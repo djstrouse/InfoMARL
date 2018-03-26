6135cd95ea2c7b230a82fef2e3ef7edbc7b8853f
DJ Strouse <danieljstrouse@gmail.com>
2018-03-26 16:28:44 -0400

    added wall penalty and random transitions

--- env_config.py
+++ env_config.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-experiment_name_ext = '_5x5'
+experiment_name_ext = '_5x6'
 
 # TwoGoalGridWorld environment variables
 EnvParam = namedtuple('EnvironmentParameters',
@@ -8,12 +8,16 @@ EnvParam = namedtuple('EnvironmentParameters',
                       'r_correct',
                       'r_incorrect',
                       'r_step',
+                      'r_wall',
+                      'p_rand',
                       'goal_locs',
                       'goal_dist'])
-env_param = EnvParam(shape = [5,5],
+env_param = EnvParam(shape = [7,5],
                      r_correct = +1,
                      r_incorrect = -1,
                      r_step = 0.,
+                     r_wall = -.1,
+                     p_rand = 0.,
                      goal_locs = None,
                      goal_dist = None)
 
